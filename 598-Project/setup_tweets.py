f2299ae2df06d675dd8e2d996a667e3bbf26d250
simaana7 <simaana7@users.noreply.github.com>
2021-11-14 16:06:35 -0500

    Setup env

---
+++ 598-Project/setup_tweets.py
@@ -0,0 +1,40 @@
+import requests
+# For saving access tokens and for file management when creating and adding to the dataset
+import os
+# For dealing with json responses we receive from the API
+import json
+# For displaying the data after
+import pandas as pd
+# For saving the response data in CSV format
+import csv
+# For parsing the dates received from twitter in readable formats
+import datetime
+import dateutil.parser
+import unicodedata
+#To add wait time between requests
+import time
+
+os.environ['TOKEN'] = '<AAAAAAAAAAAAAAAAAAAAABoqVwEAAAAAKgjI3zkKrVI0da9rgGh7UsdvgFU%3D9DdZRJ1YpJrYXcfauu7jtxxB3Mmn9W5Oj1XZhfXAn5ICafhhoc>'
+
+def auth():
+    return os.getenv('TOKEN')
+
+def create_headers(bearer_token):
+    headers = {"Authorization": "Bearer {}".format(bearer_token)}
+    return headers
+
+
+def create_url(keyword, start_date, end_date, max_results=10):
+    search_url = "https://api.twitter.com/2/tweets/search/all"  # Change to the endpoint you want to collect data from
+
+    # change params based on the endpoint you are using
+    query_params = {'query': keyword,
+                    'start_time': start_date,
+                    'end_time': end_date,
+                    'max_results': max_results,
+                    'expansions': 'author_id,in_reply_to_user_id,geo.place_id',
+                    'tweet.fields': 'id,text,author_id,in_reply_to_user_id,geo,conversation_id,created_at,lang,public_metrics,referenced_tweets,reply_settings,source',
+                    'user.fields': 'id,name,username,created_at,description,public_metrics,verified',
+                    'place.fields': 'full_name,id,country,country_code,geo,name,place_type',
+                    'next_token': {}}
+    return (search_url, query_params)
