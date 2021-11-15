f712fb13a4fa62db0b2a79817524133a7bb160ea
simaana7 <simaana7@users.noreply.github.com>
2021-11-15 10:07:48 -0500

    - Need to fix error 403

--- 598-Project/setup_tweets.py
+++ 598-Project/setup_tweets.py
@@ -14,7 +14,7 @@ import unicodedata
 #To add wait time between requests
 import time
 
-os.environ['TOKEN'] = '<AAAAAAAAAAAAAAAAAAAAABoqVwEAAAAAKgjI3zkKrVI0da9rgGh7UsdvgFU%3D9DdZRJ1YpJrYXcfauu7jtxxB3Mmn9W5Oj1XZhfXAn5ICafhhoc>'
+os.environ['TOKEN'] = 'AAAAAAAAAAAAAAAAAAAAABtFVwEAAAAA4Y1LY4TnIWv67yp3XUaqvgYdqIo%3DIPZ1Xa0d0xuYpzT9BGCU9TUOZY3xqBPPrj3ndi9x78k3JGV1hm'
 
 def auth():
     return os.getenv('TOKEN')
@@ -38,3 +38,24 @@ def create_url(keyword, start_date, end_date, max_results=10):
                     'place.fields': 'full_name,id,country,country_code,geo,name,place_type',
                     'next_token': {}}
     return (search_url, query_params)
+
+def connect_to_endpoint(url, headers, params, next_token = None):
+    params['next_token'] = next_token   #params object received from create_url function
+    response = requests.request("GET", url, headers = headers, params = params)
+    print("Endpoint Response Code: " + str(response.status_code))
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+
+bearer_token = auth()
+headers = create_headers(bearer_token)
+keyword = "(COVID OR covid-19 OR SARS-CoV-2 OR coronavirus OR #COVID OR #VACCINATION) AND lang:en"
+start_time =  "2021-11-12T14:00:00.000Z"
+end_time = "2021-11-15T14:00:00.000Z"
+max_results = 10
+
+url = create_url(keyword, start_time,end_time, max_results)
+json_response = connect_to_endpoint(url[0], headers, url[1])
+
+print(json.dumps(json_response, indent=4, sort_keys=True))
